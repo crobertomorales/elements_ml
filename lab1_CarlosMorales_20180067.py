0a410f4293031034ae6323aa891b959572b857a2
crobertomorales <68971014+crobertomorales@users.noreply.github.com>
2021-01-21 17:27:42 -0600

    Add files via upload

---
+++ lab1_CarlosMorales_20180067.py
@@ -0,0 +1,13 @@
+def ope(n,m):
+    total = 0
+    while
+    if (n == m):
+        total = total + 1
+    if (m == 0):
+        total = total + 1
+    if (m > 0 and n > m):
+        n = n-1
+        m = m-1
+        total = total + 1
+    return(total)
+print(ope(3,1))
